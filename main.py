2608d886d672929ba3093411aeda45d94f7bbfe6
sepandhaghighi <sepand.haghighi@yahoo.com>
2017-12-23 02:24:43 +0330

    feat : Integer Multipication Algorithm Added

--- main.py
+++ main.py
@@ -44,6 +44,43 @@ def IntegerSquaring(Input_integer):
     d=list(map(str,d))
     return int("".join(d))
 
+def IntegerMultiplication(Input_integer_1,Input_integer_2):
+    IntegerList1 = list(map(int, list(str(Input_integer_1))))
+    IntegerList2 = list(map(int, list(str(Input_integer_2))))
+    IntegerList1.reverse()
+    IntegerList2.reverse()
+    IntegerLength = max(len(IntegerList1),len(IntegerList2))
+    IntegerList1.extend([0]*(IntegerLength-len(IntegerList1)))
+    IntegerList2.extend([0] * (IntegerLength - len(IntegerList2)))
+    I = []
+    UHL=0
+    d=[]
+    for k in range((2*IntegerLength-2)+1):
+        if k<IntegerLength:
+            I = list(range(k+1))
+        else:
+            I=list(range(k-IntegerLength+1,IntegerLength))
+        for i in I:
+            UHL = UHL + IntegerList1[i] * IntegerList2[k - i]
+        L = UHL % 10
+        U = UHL // 100
+        H = (UHL - U * 100 - L) // 10
+        d.append(L)
+        L = H
+        H = U
+        U = 0
+        UHL = H*10+L
+    if L!=0:
+        d.append(L)
+    d.reverse()
+    d = list(map(str, d))
+    return int("".join(d))
+
+
+
+
+
+
 
 if __name__=="__main__":
     doctest.testfile("test.py", optionflags=doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS, verbose=True)
