66bc9267dabcfef9a1601fa1954cf519451ce757
sepandhaghighi <sepand.haghighi@yahoo.com>
2017-12-25 15:06:16 +0330

    fix : functions input format fixed

--- main.py
+++ main.py
@@ -6,44 +6,53 @@ import doctest
 '''
 def IntegerSquaring(Input_integer,Base=10):
     '''
-    Integer Squaring Algorithm
-    Book : Cryptographic Engineering (Serdar S¨ uer Erdem, Tuˇ grul Yanık, and C ¸ etin Kaya Koc)
-    Chapter : 5
-    Page : 80
+     Integer Squaring Algorithm
+     Book : Cryptographic Engineering (Serdar S¨ uer Erdem, Tuˇ grul Yanık, and C ¸ etin Kaya Koc)
+     Chapter : 5
+     Page : 80
+    :param Input_integer : Input Integer as string with space separation between digit "0 11 12" in base 13
+    :type Input_integer : str
     '''
-    IntegerList=list(map(int,list(str(Input_integer))))
-    IntegerList.reverse()
-    IntegerLength=len(IntegerList)
-    I=[]
-    d=[]
-    UHL=0
-    UHLPrev=0
-    for k in range((2*IntegerLength-2)+1):
-        if k<IntegerLength:
-            I=list(range(int(math.ceil(k/2))))
-        else:
-            I=list(range((k//2)+1,IntegerLength))
-        for i in I:
-            UHL=UHL+IntegerList[i]*IntegerList[k-i]
-        if (k%2)==0:
-            UHL=2*UHL+IntegerList[int(k/2)]**2
-        else:
-            UHL=2*UHL
-        UHL=UHL+UHLPrev
-        L=UHL%Base
-        U=UHL//(Base**2)
-        H=(UHL-U*(Base**2)-L)//Base
-        d.append(L)
-        L=H
-        H=U
-        U=0
-        UHLPrev=H*Base+L
+    try:
+        IntegerList=list(map(int,list(Input_integer.split(" "))))
+        for number in IntegerList:
+            if number>Base-1:
+                raise Exception("[Erro] Bad Number In This Base")
+        IntegerList.reverse()
+        IntegerLength=len(IntegerList)
+        I=[]
+        d=[]
         UHL=0
-    if L!=0:
-        d.append(L)
-    d.reverse()
-    d=list(map(str,d))
-    return int("".join(d))
+        UHLPrev=0
+        for k in range((2*IntegerLength-2)+1):
+            if k<IntegerLength:
+                I=list(range(int(math.ceil(k/2))))
+            else:
+                I=list(range((k//2)+1,IntegerLength))
+            for i in I:
+                UHL=UHL+IntegerList[i]*IntegerList[k-i]
+            if (k%2)==0:
+                UHL=2*UHL+IntegerList[int(k/2)]**2
+            else:
+                UHL=2*UHL
+            UHL=UHL+UHLPrev
+            L=UHL%Base
+            U=UHL//(Base**2)
+            H=(UHL-U*(Base**2)-L)//Base
+            d.append(L)
+            L=H
+            H=U
+            U=0
+            UHLPrev=H*Base+L
+            UHL=0
+        if L!=0:
+            d.append(L)
+        d.reverse()
+        d=list(map(str,d))
+        return " ".join(d)
+    except Exception as e:
+        print(str(e))
+        return 0
 
 def IntegerMultiplication(Input_integer_1,Input_integer_2,Base=10):
     '''
@@ -51,9 +60,13 @@ def IntegerMultiplication(Input_integer_1,Input_integer_2,Base=10):
     Book : Cryptographic Engineering (Serdar S¨ uer Erdem, Tuˇ grul Yanık, and C ¸ etin Kaya Koc)
     Chapter : 5
     Page : 79
+    :param Input_integer_1 : Input Integer as string with space separation between digit "0 11 12" in base 13
+    :param Input_integer_2 : Input Integer as string with space separation between digit "0 11 12" in base 13
+    :type Input_integer_1 : str
+    :type Input_integer_2 : str
     '''
-    IntegerList1 = list(map(int, list(str(Input_integer_1))))
-    IntegerList2 = list(map(int, list(str(Input_integer_2))))
+    IntegerList1 = list(map(int, list(Input_integer_1.split(" "))))
+    IntegerList2 = list(map(int, list(Input_integer_2.split(" "))))
     IntegerList1.reverse()
     IntegerList2.reverse()
     IntegerLength = max(len(IntegerList1),len(IntegerList2))
@@ -81,7 +94,7 @@ def IntegerMultiplication(Input_integer_1,Input_integer_2,Base=10):
         d.append(L)
     d.reverse()
     d = list(map(str, d))
-    return int("".join(d))
+    return " ".join(d)
 
 
 
